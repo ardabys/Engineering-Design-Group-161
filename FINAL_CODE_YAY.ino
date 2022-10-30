b89aa4e252589ed2488326e9a6e59d14285a20d9
ardabys <75312660+ardabys@users.noreply.github.com>
2022-10-30 16:04:30 +0100

    Add files via upload

--- FINAL_CODE_YAY.ino
+++ FINAL_CODE_YAY.ino
@@ -5,9 +5,10 @@ Servo servoPlastic;
 Servo servoOther;  
 
 void setup() {
-  pinMode(13, OUTPUT); //LED
+  pinMode(13, OUTPUT); //LED1
   pinMode(8, INPUT);  //METAL
   pinMode(7, INPUT);  //IR
+  pinMode(6, OUTPUT); //LED2
   servoMetal.attach(9); //Servo 1 
   servoPlastic.attach(10); //Servo 2
   servoOther.attach(11); //Servo 3
@@ -18,25 +19,25 @@ void setup() {
 void loop() {
     
   if(digitalRead(7) == LOW){  //waste is detected by IR
-    digitalWrite(13, HIGH);   //Turn LED ON
-    delay(100);
+    digitalWrite(13, HIGH);   //Turn LED1 ON
+    digitalWrite(6, HIGH);    //Turn LED2 ON
     Serial.println("Detecting...");
-    delay(2000);
+    delay(1000);
     
     if( digitalRead(8) == LOW ){ //metal is detected
       Serial.println( "Metal detected! Beginning sorting." );
       delay(1000);
       
       //TILTING FOR METAL
-      servoOther.writeMicroseconds(2000);
-      servoPlastic.writeMicroseconds(2000);
-      delay(3000); //run the servos for 3000 seconds
-      servoOther.writeMicroseconds(1500);
-      servoPlastic.writeMicroseconds(1500);
-      delay(3000); //wait for the waste to slide
       servoOther.writeMicroseconds(1000);
       servoPlastic.writeMicroseconds(1000);
-      delay(3000);  //run the servos for 3000 seconds
+      delay(2500); //run the servos for 3000 seconds
+      servoOther.writeMicroseconds(1500);
+      servoPlastic.writeMicroseconds(1500);
+      delay(2000); //wait for the waste to slide
+      servoOther.writeMicroseconds(2000);
+      servoPlastic.writeMicroseconds(2000);
+      delay(2500);  //run the servos for 3000 seconds
       servoOther.writeMicroseconds(1500);
       servoPlastic.writeMicroseconds(1500);
       
@@ -57,32 +58,24 @@ void loop() {
     Serial.println(analogRead(A0));    //prints out the light level
     delay(1000);
     int light_level =  analogRead(A0);
-
-//      ADDITIONAL SCENARIO(too much light --> lid is open)
-//      This scenario can be implemented as a general check for the program (if the lid is open --> too much light --> no checking)
-//      if(analogValue > treshold2) {
-//        println("Please close the lid to begin detection and sorting");
-//        delay(3000);
-//        return;
-//      }
       
 
       //NON PLASTIC WASTE DETECTION
       
-    if(light_level < 300) {  //treshold to be determined by testing
+    if(light_level > 800) {  //treshold to be determined by testing
       Serial.println("Non plastic waste detected. Beginning sorting.");
 
       //TILTING FOR NON PLASTIC
       
-      servoPlastic.writeMicroseconds(2000);
-      servoMetal.writeMicroseconds(2000);
-      delay(3000); //run the servos for 3000 seconds
+      servoPlastic.writeMicroseconds(1000);
+      servoMetal.writeMicroseconds(1000);
+      delay(2500); //run the servos for 3000 seconds
       servoPlastic.writeMicroseconds(1500);
       servoMetal.writeMicroseconds(1500);
-      delay(3000); //wait for the waste to slide
-      servoMetal.writeMicroseconds(1000);
-      servoPlastic.writeMicroseconds(1000);
-      delay(3000);  //run the servos for 3000 seconds
+      delay(2000); //wait for the waste to slide
+      servoMetal.writeMicroseconds(2000);
+      servoPlastic.writeMicroseconds(2000);
+      delay(2500);  //run the servos for 3000 seconds
       servoMetal.writeMicroseconds(1500);  //stop servos
       servoPlastic.writeMicroseconds(1500);
       Serial.println("Done.");
@@ -94,15 +87,15 @@ void loop() {
       
     else {
       Serial.println("Plastic waste detected! Beginning sorting.");
-      servoOther.writeMicroseconds(2000);
-      servoMetal.writeMicroseconds(2000);
-      delay(3000); //run the servos for 3000 seconds
-      servoOther.writeMicroseconds(1500);
-      servoMetal.writeMicroseconds(1500);
-      delay(3000); //wait for the waste to slide
       servoOther.writeMicroseconds(1000);
       servoMetal.writeMicroseconds(1000);
-      delay(3000);  //run the servos for 3000 seconds
+      delay(2500); //run the servos for 3000 seconds
+      servoOther.writeMicroseconds(1500);
+      servoMetal.writeMicroseconds(1500);
+      delay(2000); //wait for the waste to slide
+      servoOther.writeMicroseconds(2000);
+      servoMetal.writeMicroseconds(2000);
+      delay(2500);  //run the servos for 3000 seconds
       servoMetal.writeMicroseconds(1500); //Stop servos
       servoOther.writeMicroseconds(1500);
       Serial.println("Done.");
@@ -110,7 +103,7 @@ void loop() {
     }
   }
     else {  //no waste is detected by IR
-      digitalWrite(13, LOW);
-      delay(1000);
+      digitalWrite(13, LOW);   //Turn LED1 OFF
+      digitalWrite(6, LOW);    //Turn LED2 OFF
     }
 }
